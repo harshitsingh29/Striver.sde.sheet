df325d48c2139f6e9c6f6c166885fd095cb3916a
harshitsingh29 <harsh.official29@gmail.com>
2021-07-02 19:54:19 +0530

    Create job sequencing problem.cpp

---
+++ job sequencing problem.cpp
@@ -0,0 +1,35 @@
+// link - https://practice.geeksforgeeks.org/problems/job-sequencing-problem-1587115620/1#
+
+
+// { Driver Code Starts
+// Program to find the maximum profit job sequence from a given array 
+// of jobs with deadlines and profits 
+#include<bits/stdc++.h>
+using namespace std; 
+
+// A structure to represent a job 
+struct Job 
+{ 
+    int id;	 // Job Id 
+    int dead; // Deadline of job 
+    int profit; // Profit if job is over before or on deadline 
+}; 
+
+
+ // } Driver Code Ends
+
+/*
+struct Job 
+{ 
+    int id;	 // Job Id 
+    int dead; // Deadline of job 
+    int profit; // Profit if job is over before or on deadline 
+};
+*/
+
+class Solution 
+{
+    public:
+    //Function to find the maximum profit and the number of jobs done.
+    static bool comparator(Job j1 , Job j2 )
+    {
